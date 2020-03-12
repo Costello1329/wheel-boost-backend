b119c2877198e03d9fcc38747a1a920a41742e2b
Konstantin Leladze <leladze.kg@phystech.edu>
2020-03-12 18:59:41 +0300

    added model

--- backend/models.py
+++ backend/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Event(models.Model):
+    title = models.CharField(max_length=50)
+    description = models.TextField()
+    startTime = models.DateTimeField()
+    endTime = models.DateTimeField()
+    price = models.IntegerField()
+    peopleCount = models.IntegerField()
