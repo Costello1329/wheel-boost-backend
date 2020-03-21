1879794bdc3d74761b96aed4844444f893917343
Kirill <semennikov1360@mail.ru>
2020-03-21 22:05:41 +0300

    add dynamic radius

--- get_events_service/apps.py
+++ get_events_service/apps.py
@@ -3,3 +3,12 @@ from django.apps import AppConfig
 
 class GetEventsServiceConfig(AppConfig):
     name = 'get_events_service'
+
+
+class Driver:
+    latitude: float
+    longitude: float
+
+    def __init__(self, driver_latitude, driver_longitude):
+        self.latitude = driver_latitude
+        self.longitude = driver_longitude
--- get_events_service/views.py
+++ get_events_service/views.py
@@ -2,14 +2,21 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 import datetime
 from math import sin, cos, acos, pi
+
+from get_events_service.apps import Driver
 from main.models import Event
 
 k_nearest_events_radius_in_kms = 5
+k_expansion_step_in_kms = 1
+k_max_radius = 15
+
+initial_time_deviation = 0.5  # -0.5 hour
+final_time_deviation = 3  # +3 hour
 
 
 # TODO: Use more time-driven metrics:
 def get_distance_in_kms(first_latitude, first_longitude, second_latitude, second_longitude):
-    r = 6371 # earth's radius.
+    r = 6371  # earth's radius.
     first_latitude *= pi / 180
     second_latitude *= pi / 180
     first_longitude *= pi / 180
@@ -21,30 +28,23 @@ def get_distance_in_kms(first_latitude, first_longitude, second_latitude, second
     )
 
 
-def is_this_event_near(driver_latitude, driver_longitude, event_latitude, event_longitude):
+def is_this_event_near(driver_latitude, driver_longitude, event_latitude, event_longitude,radius):
     return get_distance_in_kms(
         driver_latitude,
         driver_longitude,
         event_latitude,
         event_longitude
-    ) < k_nearest_events_radius_in_kms
+    ) < radius
 
 
-class UserView(APIView):
-    def post(self, request):
-        coordinates = request.data["coordinates"]
-        driver_latitude, driver_longitude = coordinates.split(";")
-        driver_latitude, driver_longitude = float(driver_latitude), float(driver_longitude)
-        now = datetime.datetime.now()
-        end_date = now + datetime.timedelta(days=1)
-        events = Event.objects.filter(endTime__gte=now, endTime__lte=end_date)
-        events_out = []
-
+def get_nearest_event(events, driver):
+    events_out = []
+    for current_radius in range(k_nearest_events_radius_in_kms, k_max_radius, k_expansion_step_in_kms):
         for event in events:
             event_latitude, event_longitude = event.coordinates.split(";")
             event_latitude, event_longitude = float(event_latitude), float(event_longitude)
 
-            if is_this_event_near(driver_latitude, driver_longitude, event_latitude, event_longitude):
+            if is_this_event_near(driver.latitude, driver.longitude, event_latitude, event_longitude,current_radius):
                 event_dict = {
                     "title": event.title,
                     "coordinates": event.coordinates,
@@ -55,9 +55,22 @@ class UserView(APIView):
                     "peopleCount": event.peopleCount
                 }
                 events_out.append(event_dict)
+                if len(events_out) != 0:
+                    return events_out
+    return events_out
 
+
+class UserView(APIView):
+    def post(self, request):
+        coordinates = request.data["coordinates"]
+        driver_latitude, driver_longitude = coordinates.split(";")
+        driver_latitude, driver_longitude = float(driver_latitude), float(driver_longitude)
+        start_date = datetime.datetime.now() - datetime.timedelta(hours=initial_time_deviation)
+        end_date = datetime.datetime.now() + datetime.timedelta(hours=final_time_deviation)
+        events = Event.objects.filter(endTime__gte=start_date, endTime__lte=end_date)
+        driver = Driver(driver_latitude, driver_longitude)
+        events_out = get_nearest_event(events, driver)
         body = {
             "events": events_out
         }
-
         return Response(body, status=200, content_type="application/json")
